2d7850880296c91cb42a179a6215ad1c44389ce4
Marek Discantiny <discantiny@united-domains.de>
2013-12-02 14:07:55 +0100

    PageRenderer interface defined

---
+++ PageRenderer.php
@@ -0,0 +1,15 @@
+<?php
+
+class PageRenderer {
+    
+    public function __construct(ArrayIterator $data) {
+        
+    }
+    
+    /**
+     * @return string
+     */
+    public function render() {
+        
+    }
+}
