d16227df77705cba04bf1347bd48d4c6d13eec80
Christian Wolf <wolf@united-domains.de>
2013-12-02 14:25:08 +0100

    DataReader stub

---
+++ DataReader.php
@@ -0,0 +1,25 @@
+<?php
+class DataReader {
+	/**
+	 * new instance of Datareader
+	 * 
+	 * @param string  $filename
+	 * @param integer $page_length
+	 * @param integer $offest optional
+	 * 
+	 * @return void
+	 */
+	public function __construct($filename, $page_length, $offset = 0) {
+		
+	}
+	
+	/**
+	 * get array of row strings
+	 * 
+	 * @return array
+	 */
+	public function getRows() {
+		return array();
+	}
+}
+
