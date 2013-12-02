d876bffb91036b8ddae0e08ef38b7d3662ae88e9
Marek Discantiny <discantiny@united-domains.de>
2013-12-02 15:25:43 +0100

    codesniffer

--- DataParser.php
+++ DataParser.php
@@ -1,34 +1,34 @@
 <?php
 
 class DataParser {
-    
+
     const RECORD_DELIM = ';';
-    
+
     /**
      * @var array
      */
     private $lines;
-    
+
     /**
      * @param array $lines
      */
     public function __construct($lines) {
         $this->lines = $lines;
     }
-    
+
     /**
      * @return ArrayIterator
      */
     public function getPage() {
         $result = new ArrayIterator();
-        
+
         foreach ($this->lines as $line) {
             $result->append($this->_parseLine($line));
         }
-        
+
         return $result;
     }
-    
+
     /**
      * @param string $line
      *
