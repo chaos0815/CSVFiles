57b97ebdd5f7006ae03f727352e5a96615351f6d
Heiko Schorstein <schorstein@united-domains.de>
2013-12-03 14:41:47 +0100

    added Commandline class

--- CommandLine.php
+++ CommandLine.php
@@ -1,5 +1,33 @@
 <?php
 class CommandLine {
+
+    /**
+     * @var array
+     */
+    private $_commandline_args = array();
+
+    public function __construct() {
+        $this->_getInitialArgs();
+    }
+
+    /**
+     * gets commandline args and sets class members accordingly
+     *
+     * @return void
+     */
+    private function getInitialArgs() {
+        global $argv;
+        $this->_commandline_args['filename'] = $argv[1];
+    }
+
+    /**
+     * returns the filename given through commandline param
+     *
+     * @return string
+     */
+    public function extractFilename() {
+        return $this->_commandline_args['filename'];
+    }
 }
 
 ?>
