3bdee108d8e8e51ed119d6d0424d9a0a8fbc3192
Christian Wolf <wolf@united-domains.de>
2013-12-04 13:20:29 +0100

    question form

--- AppFrage/QuestionnaireForm.php
+++ AppFrage/QuestionnaireForm.php
@@ -1,10 +1,33 @@
 <?php
 class QuestionnaireForm extends Zend_Form {
+	private $questionnaire;
 	
 	public function init() {
+		foreach ($this->questionnaire as $question) {
+			$this->addQuestion($question);
+		}
 		
 	} 
+	
+	public function setQuestionnaire(Questionnaire $questionnaire) {
+		$this->questionnaire = $questionnaire;
+	}
 
+	private function addQuestion(Question $question) {
+		$text_element = new Zend_Form_Element_Text('qtext');
+		$text_element->setValue($question->getText())->helper = 'formNote';
+		
+		$this->addElement($text_element);
+		$this->addElement($this->getAnswers($question->getAnswers()));
+	}
+	
+	private function getAnswers($answers) {
+		$answers = new Zend_Form_Element_MultiCheckbox('answers');
+		$answers->setOptions($answers);
+		
+		return $answers;
+	}
+	
 	
 }
 
