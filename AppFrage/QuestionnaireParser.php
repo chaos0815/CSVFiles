a9aa351bb98474a2ad711d4deaadbead3899a592
Marek Discantiny <discantiny@united-domains.de>
2013-12-04 12:22:04 +0100

    QuestionnaireParser signature

---
+++ AppFrage/QuestionnaireParser.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class QuestionnaireParser {
+
+    /**
+     * @return Questionnaire
+     */
+    public function parseQuestionnaire() {
+        return new Questionnaire(array());
+    }
+}
