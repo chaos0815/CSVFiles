db9559ed58ecec7576f920c84297dab960201691
Marek Discantiny <discantiny@united-domains.de>
2013-12-04 12:18:03 +0100

    Question class implemented

---
+++ AppFrage/Question.php
@@ -0,0 +1,32 @@
+<?php
+
+class Question {
+
+    private $text;
+
+    private $answers;
+
+
+    /**
+     * @param string $text
+     * @param array  $answers
+     */
+    public function __construct($text, $answers) {
+        $this->text    = $text;
+        $this->answers = $answers;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText() {
+        return $this->text;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAnswers() {
+        return $answers;
+    }
+}
